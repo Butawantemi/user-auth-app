c91e8fe1b438a83ecccaabb8a3d619ba14de1e06
Butawantemi <japhetbuta@gmail.com>
2024-07-05 17:24:57 +0300

    use sequelize to connect database

--- api/index.js
+++ api/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const sequelize = require('./config/dbConnect');
+require('dotenv').config()
 
 const app = express()
+
+//Middleware
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4000;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-})
+sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}).catch(err => console.log(err));
 
 module.exports = app;
