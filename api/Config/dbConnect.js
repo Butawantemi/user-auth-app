af46ec541125f776cfdca05fa1cf054158ac01ea
Butawantemi <japhetbuta@gmail.com>
2024-07-07 11:50:59 +0300

    fix bug & add some missing comment

--- api/Config/dbConnect.js
+++ api/Config/dbConnect.js
@@ -1,8 +1,15 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config()
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help enforce SSL connection
+      rejectUnauthorized: false // If you are facing issues with self-signed certificates
+    }
+  }
 });
 
 module.exports = sequelize;
+
