c4a929d27a37bfffb3af24617a004c9a01fbef1f
Butawantemi <japhetbuta@gmail.com>
2024-07-08 11:23:51 +0300

    fix bug & add some missing comment

--- api/Routes/org.route.js
+++ api/Routes/org.route.js
@@ -1,17 +1,28 @@
-const express = require('express');
-const { body } = require('express-validator');
-const { authenticateToken } = require('../Middleware/authToken');
-const { getOrganisations, getOrganisation, createOrganisation, addUserToOrganisation } = require('../Controllers/org.controller');
+const express = require("express");
+const { body } = require("express-validator");
+const { authenticateToken } = require("../Middleware/authToken");
+const {
+  getOrganisations,
+  getOrganisation,
+  createOrganisation,
+  addUserToOrganisation,
+} = require("../Controllers/org.controller");
 
 const router = express.Router();
 
-router.get('/', authenticateToken, getOrganisations);
-router.get('/:orgId', authenticateToken, getOrganisation);
-router.post('/', authenticateToken, [
-  body('name').notEmpty().withMessage('Organisation name is required'),
-], createOrganisation);
-router.post('/:orgId/users', authenticateToken, [
-  body('userId').notEmpty().withMessage('User ID is required'),
-], addUserToOrganisation);
+router.get("/", authenticateToken, getOrganisations);
+router.get("/:orgId", authenticateToken, getOrganisation);
+router.post(
+  "/",
+  authenticateToken,
+  [body("name").notEmpty().withMessage("Organisation name is required")],
+  createOrganisation
+);
+router.post(
+  "/:orgId/users",
+  authenticateToken,
+  [body("userId").notEmpty().withMessage("User ID is required")],
+  addUserToOrganisation
+);
 
 module.exports = router;
